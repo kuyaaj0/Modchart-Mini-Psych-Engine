2324e0155d9b73e2d3d69c85f7e0c499083ee9fe
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-16 01:10:03 -0700

    Add ModifierOutput class with properties

---
+++ Src/Modchart/Backend/Core/ModifierOutput.hx
@@ -0,0 +1,12 @@
+package Src.Modchart.Backend.Core;
+
+#if !openfl_debug
+@:fileXml('tags="haxe,release"')
+@:noDebug
+#end
+@:structInit
+@:publicFields
+class ModifierOutput {
+	var pos:Vector3;
+	var visuals:VisualParameters;
+}
