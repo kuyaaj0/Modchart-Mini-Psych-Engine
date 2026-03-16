1e9e495661d1fb0878e368f8794f19beaf3aca87
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-17 06:25:39 +0800

    Create Vibrate.hx

---
+++ Src/Modchart/Engine/Modifier/List/False_Paradise/Vibrate.hx
@@ -0,0 +1,17 @@
+package Modchart.Engine.Modifier.List.False_Paradise;
+
+import Modchart.Backend.Core.ArrowData;
+import Modchart.Backend.Core.ModifierParameters;
+
+class Vibrate extends Modifier {
+	override public function render(curPos:Vector3, params:ModifierParameters) {
+		var vib = getPercent('vibrate', params.player);
+		curPos.x += (Math.random() - 0.5) * vib * 20;
+		curPos.y += (Math.random() - 0.5) * vib * 20;
+
+		return curPos;
+	}
+
+	override public function shouldRun(params:ModifierParameters):Bool
+		return getPercent('vibrate', params.player) != 0;
+}
