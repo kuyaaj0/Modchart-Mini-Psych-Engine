4df6e7143cadcd5376b16f9879782f1c9dbd82aa
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-17 04:21:20 +0800

    Create ArrowShape.hx

---
+++ Src/Modchart/Engine/Modifier/List/ArrowShape.hx
@@ -0,0 +1,35 @@
+package Modchart.Engine.Modifier.List;
+
+import Modchart.Backend.Core.ArrowData;
+import Modchart.Backend.Parameter.ModifierParameters;
+import Modchart.Engine.PlayField;
+import Modchart.Engine.Modifier.List.PathModifier.PathNode;
+
+class ArrowShape extends PathModifier {
+	public function new(pf:PlayField) {
+		var path:Array<PathNode> = [];
+
+		for (line in ModchartUtil.coolTextFile('Modchart/arrowShape.csv')) {
+			var coords = line.split(';');
+			path.push({
+				x: Std.parseFloat(coords[0]) * 200,
+				y: Std.parseFloat(coords[1]) * 200,
+				z: Std.parseFloat(coords[2]) * 200
+			});
+		}
+
+		super(pf, path);
+
+		pathOffset.setTo(WIDTH * 0.5, HEIGHT * 0.5 + 280, 0);
+	}
+
+	override function render(pos:Vector3, params:ModifierParameters) {
+		var perc = getPercent('arrowshape', params.player);
+
+		if (perc == 0)
+			return pos;
+
+		pathOffset.z = pos.z;
+		return computePath(pos, params, perc);
+	}
+}
