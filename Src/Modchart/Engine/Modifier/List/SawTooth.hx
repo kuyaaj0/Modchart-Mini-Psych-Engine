e397e18f2f9e68d60581188f6b1025c1bae0df0f
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-17 05:02:58 +0800

    Create SawTooth.hx

---
+++ Src/Modchart/Engine/Modifier/List/SawTooth.hx
@@ -0,0 +1,18 @@
+package Modchart.Engine.Modifier.List;
+
+import Modchart.Backend.Core.ArrowData;
+import Modchart.Backend.Parameter.ModifierParameters;
+
+class SawTooth extends Modifier {
+	override public function render(curPos:Vector3, params:ModifierParameters) {
+		var player = params.player;
+		final period = 1 + getPercent("sawtoothPeriod", player);
+		curPos.x += (getPercent('sawtooth',
+			player) * ARROW_SIZE) * ((0.5 / period * params.distance) / ARROW_SIZE - Math.floor((0.5 / period * params.distance) / ARROW_SIZE));
+
+		return curPos;
+	}
+
+	override public function shouldRun(params:ModifierParameters):Bool
+		return true;
+}
