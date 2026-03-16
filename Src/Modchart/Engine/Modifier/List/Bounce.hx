457c294da3c3a3077e13841d1843ed0338d4eab9
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-17 04:25:31 +0800

    Create Bounce.hx

---
+++ Src/Modchart/Engine/Modifier/List/Bounce.hx
@@ -0,0 +1,23 @@
+package Modchart.Engine.Modifier.List;
+
+import Modchart.Backend.Core.ArrowData;
+import Modchart.Backend.Parameter.ModifierParameters;
+
+class Bounce extends Modifier {
+	override public function render(curPos:Vector3, params:ModifierParameters) {
+		var player = params.player;
+		var speed = getPercent('bounceSpeed', player);
+		var offset = getPercent('bounceOffset', player);
+
+		var bounce = Math.abs(sin((params.curBeat + offset) * (1 + speed) * Math.PI)) * ARROW_SIZE;
+
+		curPos.x += bounce * getPercent('bounceX', player);
+		curPos.y += bounce * (getPercent('bounce', player) + getPercent('bounceY', player));
+		curPos.z += bounce * getPercent('bounceZ', player);
+
+		return curPos;
+	}
+
+	override public function shouldRun(params:ModifierParameters):Bool
+		return true;
+}
