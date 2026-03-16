8b581df5110819fd3c57696183c461f33e8c3f0e
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-17 05:00:37 +0800

    Create Rotate.hx

---
+++ Src/Modchart/Engine/Modifier/List/Rotate.hx
@@ -0,0 +1,37 @@
+package Modchart.Engine.Modifier.List;
+
+import flixel.FlxG;
+import Modchart.Backend.Core.ArrowData;
+import Modchart.Backend.Parameter.ModifierParameters;
+import Backend.Utils.ModchartUtil;
+
+class Rotate extends Modifier {
+	override public function render(curPos:Vector3, params:ModifierParameters) {
+		var rotateName = getRotateName();
+		var player = params.player;
+
+		var angleX = getPercent(rotateName + 'X', player);
+		var angleY = getPercent(rotateName + 'Y', player);
+		var angleZ = getPercent(rotateName + 'Z', player);
+
+		// does angleY work here if angleX and angleZ are disabled? - ye
+		if (angleX == 0 && angleY == 0 && angleZ == 0)
+			return curPos;
+
+		final origin:Vector3 = getOrigin(curPos, params);
+		curPos = ModchartUtil.rotate3DVector(curPos -= origin, angleX, angleY, angleZ);
+		curPos += origin;
+		return curPos;
+	}
+
+	public function getOrigin(curPos:Vector3, params:ModifierParameters):Vector3 {
+		var fixedLane = Math.round(getKeyCount(params.player) * .5);
+		return new Vector3(getReceptorX(fixedLane, params.player), FlxG.height / 2);
+	}
+
+	public function getRotateName():String
+		return 'rotate';
+
+	override public function shouldRun(params:ModifierParameters):Bool
+		return true;
+}
