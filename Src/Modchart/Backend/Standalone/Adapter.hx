917b335679558e8fd1a06e68363d2911dc8ee451
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-16 19:06:08 -0700

    Create Adapter.hx

---
+++ Src/Modchart/Backend/Standalone/Adapter.hx
@@ -0,0 +1,29 @@
+package Modchart.Backend.Standalone;
+
+import haxe.macro.Compiler;
+
+class Adapter {
+	public static var instance:IAdapter;
+	private static var ENGINE_NAME:String = Compiler.getDefine("FM_ENGINE");
+
+	public static function init() {
+		if (instance != null)
+			return;
+
+		final possibleClientName = ENGINE_NAME.substr(0, 1).toUpperCase() + ENGINE_NAME.substr(1).toLowerCase();
+		final adapter = Type.createInstance(Type.resolveClass('Modchart.Backend.Standalone.Adapters.${ENGINE_NAME.toLowerCase()}.' + possibleClientName), []);
+
+		#if FM_VERBOSE
+		trace('[FunkinModchart Verbose] Finding possible adapter from "Modchart.Backend.Standalone.Adapters.${ENGINE_NAME.toLowerCase()}.${possibleClientName}"');
+		#end
+
+		if (adapter == null)
+			throw 'Adapter not found for $ENGINE_NAME';
+
+		#if FM_VERBOSE
+		trace('[FunkinModchart Verbose] Found Adapter!');
+		#end
+
+		instance = adapter;
+	}
+}
