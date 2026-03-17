d82dad16c9919c864bc1047bc51df4c3c94eb5dd
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-03-17 09:06:31 +0800

    Create import.hx

---
+++ Src/Modchart/Backend/Graphics/import.hx
@@ -0,0 +1,20 @@
+package Modchart.Backend.Graphics;
+
+#if !macro
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.graphics.tile.FlxDrawTrianglesItem.DrawData;
+import flixel.math.FlxAngle;
+import flixel.math.FlxMath;
+import flixel.math.FlxMatrix;
+import haxe.ds.Vector as NativeVector;
+import Modchart.Backend.Core.ModifierOutput;
+import Modchart.Backend.Graphics.CtxRenderer;
+import Modchart.Backend.Graphics.DrawCommand;
+import openfl.Vector;
+import openfl.display.GraphicsPathCommand;
+import openfl.display.Shape;
+import openfl.geom.ColorTransform;
+import openfl.geom.Matrix;
+#end
